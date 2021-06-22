9028b731e88c8878a9bd29645e9a2379f073d989
BahaleCodes <74527555+BahaleCodes@users.noreply.github.com>
2021-06-22 11:45:14 +0200

    Updated Settings.py

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -26,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1', 'cafblog.herokuapp.com'
+]
 
 
 # Application definition
